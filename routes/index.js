34f7853a1a19c475d7b6ce3911d08d66a1657b93
Anna Casper <anna.casper3@gmail.com>
2015-06-14 12:56:51 -0600

    removed console logs

--- routes/index.js
+++ routes/index.js
@@ -9,18 +9,18 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   var departureCity = req.body.departure_city
-  console.log(departureCity);
+  // console.log(departureCity);
   var arrivalCity = req.body.arrival_city
-  console.log(arrivalCity);
+  // console.log(arrivalCity);
   var bags = req.body.bags
-  console.log(bags);
+  // console.log(bags);
   var flightClass = req.body.class
-  console.log(flightClass);
+  // console.log(flightClass);
   var wifi = req.body.wifi
-  console.log(wifi);
+  // console.log(wifi);
   var discount = req.body.discount
-  console.log(discount);
-  console.log(calculate(departureCity, arrivalCity, bags, flightClass, wifi, discount));
+  // console.log(discount);
+  // console.log(calculate(departureCity, arrivalCity, bags, flightClass, wifi, discount));
   res.render('index', { total: calculate(departureCity, arrivalCity, bags, flightClass, wifi, discount), });
 });
 
